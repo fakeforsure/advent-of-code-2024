f48c6f0b5b544f7ec3f492e3a9109c199754cfc1
fakeforsure <91992834+fakeforsure@users.noreply.github.com>
2024-11-30 22:53:14 -0800

    Update day1.py
    That was the old code, doesn't work do not use

--- day1.py
+++ day1.py
@@ -1,27 +1,42 @@
 # Advent Code - Day 1
-# By: Luc Bassaler-Merpillat
+# By: http://github.com/fakeforsure
 # On: November 30, 2024
 
 # Imports
 import pathlib
 folder_of_code = pathlib.Path(__file__).parent.resolve()
-file = open(f"{folder_of_code}/adventcode_day1.txt")
+file = open(f"{folder_of_code}/adventcode_day1.txt") ### Load file
 
-# Create list
-lines_list = []
+'''PART 1'''
+# Create two lists: one for the right side and one for the left side
+right_side = []
+left_side = []
 for line in file:
-    words = line.strip().split()
-    lines_list.append(words)
+    word = line.strip().split()
+    right_side.append(int(word[1]))
+    left_side.append(int(word[0]))
 
-# Sub num to other num
-results_list = []
-for word in lines_list:
-    result = int(word[0]) - int(word[1])
-    results_list.append(abs(result)) ### Absolute value of the result
+#Sort from smallest to largest, literally built in function
+right_side.sort()
+left_side.sort()
 
-# Sum of all results
-sum_results = sum(results_list)
-print(sum_results)
+# Find the distance between the two sides
+distance = 0
+for i in range(len(right_side)): ### Len of any side is the same tbh
+    distance += abs(right_side[i] - left_side[i])
 
-# Done! :)
+# Print distance
+print(distance)
+
+'''PART 2'''
+# Find how many times the num on the left appears in the right side
+similar = 0
+for num in left_side:
+    if num in right_side:
+        similar += num * right_side.count(num)
+
+# Print the sum of similar numbers
+print(similar)
+
+# Done :)
 file.close()
